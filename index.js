3fd62c514dcd59040520b9f24a8b6591604ee13e
sokret sathit <sokretsathit13@gmail.com>
2022-07-15 11:39:11 -0500

    New add, JS, HTML and CSS

---
+++ index.js
@@ -0,0 +1,38 @@
+const form = document.addItem
+removeToDo = document.querySelectorAll('.removeBtn')
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const groceryItem = form.title.value
+    form.title.value = ""
+    console.log(groceryItem)
+    
+    const ul = document.getElementById("list")
+    const listItem = document.createElement("li")
+    listItem.textContent = groceryItem
+    ul.appendChild(listItem)
+    listItem.className = "listItem"
+
+    //Remove button
+    const removeButton = document.createElement("button")
+    removeButton.textContent = "X"
+    listItem.appendChild(removeButton)
+
+    //Remove button eventlistener
+removeButton.addEventListener('click', function() {
+    //var li = this.parentNode
+    //li.remove()
+    listItem.remove()
+
+    //Edit button - user input should pop-up with the list Item already on
+const editButton = document.createElement("button")
+    editButton.textContent = "Edit"
+    listItem.appendChild(editButton)
+//Edit button eventListener
+editButton.addEventListener('click', function() {
+    listItem.ed
+})
+})    
+})
